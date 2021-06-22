b080b5aa6ff3c06d6719a8083dcf7566e90e5bed
Dan Haub <haub@chapman.edu>
2021-06-22 08:04:48 -0700

    tried to work on GPP

--- GPP/GPP_Dehaze.m
+++ GPP/GPP_Dehaze.m
@@ -1,15 +1,17 @@
 clear
 
-I = imread ('../images/test.png');
-I = rgb2gray(I);
+I = imread ('../images/98.bmp');
 I = double(I) / 255;
 
 [Ig, Vr] = gradient_profile_prior(I);
 
-gamma = 
+A = max(I, [], 3);
 
 figure(1);
-subplot(2,1,1);
+subplot(2,3,1);
+imshow(I);
+subplot(2,3,2);
 imshow(Ig);
-subplot(2,1,2);
-imshow(Vr);
+subplot(2,3,3);
+imshow(A);
+
--- GPP/gradient_profile_prior.m
+++ GPP/gradient_profile_prior.m
@@ -1,4 +1,5 @@
 function [Ig, Vr] = gradient_profile_prior(I)
+    I = rgb2gray(I);
     [Ix, Iy] = imgradientxy(I);
 
     Ig = sqrt(Ix .^ 2 + Iy .^ 2);
